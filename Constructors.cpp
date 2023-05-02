071d50a18229a86da1c41f623199e4eb23958113
Mehran Alidoost Nia <24442175+alidoostnia@users.noreply.github.com>
2023-05-02 19:48:38 +0330

    partial edit

--- Constructors.cpp
+++ Constructors.cpp
@@ -6,18 +6,19 @@ class integer
 public:
   int x, y;
 
-//Default Constructor
+// Default Constructor
   integer()
   {
     x = 45;
     y = 10;
   }
+
 };
 
 int main()
 {
   // Default constructor called automatically when the object is created
   integer a;
-  cout << "x: " << a.x << endl << "y: " << a.y;
+  cout << "**** Default constructor example output **** \nx: " << a.x << endl << "y: " << a.y;
   return 0;
 }
