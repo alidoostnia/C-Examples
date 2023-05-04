83b52d6daf23707ee262ed571b3c43845057b72f
Mehran Alidoost Nia <24442175+alidoostnia@users.noreply.github.com>
2023-05-05 01:17:08 +0330

    bubble sort added

--- Sorts.cpp
+++ Sorts.cpp
@@ -107,6 +107,26 @@ void InsertionSort(int arr[], int arr_size){
   }
 }
 
+//bubble sort
+void BubbleSort(int *array, int n) {
+  bool swapped = true;
+  int j = 0;
+  int temp;
+
+  while (swapped) {
+    swapped = false;
+    j++;
+    for (int i = 0; i < n - j; ++i) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+}
+
 
 int main() {
   int array[] = {94, 42, 50, 95, 333, 65, 54, 456, 1, 1234};
